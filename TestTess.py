bc51e276df3c7667563b81352d3fa0254d4d906c
garyfeng <gary.feng@gmail.com>
2013-12-17 20:09:01 -0500

    rewote testtess.py
    just to be more clean

--- TestTess.py
+++ TestTess.py
@@ -1,106 +1,19 @@
 import cv2.cv as cv
-import tesseract
+#import tesseract
 import cv2
 import os
 import sys
 import numpy as np
-import atexit
-import glob
 
-from HTMLParser import HTMLParser
+import TessEngine 
+#from HTMLParser import HTMLParser
 from htmlentitydefs import name2codepoint
 
-# how much magnification for Tesseract to do OCR, typically 3-5
-ratio=3.0
-# where the text is. Default the whole page
-w= -1
-h= -1
-x= -1
-y= -1
-colorPlane=-1
-os.chdir(os.path.dirname(sys.argv[0]))
-try:
-    frameFileName = sys.argv[1]
-except:
-    print "testtess.py frameFileName [x y w h] [colorPlane] [ratio]"
-    exit(0)
-    
-try:
-    x = int(sys.argv[2])
-    y = int(sys.argv[3])
-    w = int(sys.argv[4])
-    h = int(sys.argv[5])
-except:
-    print "[x y w h] not specified; using the whole page"
-
-try:
-    colorPlane = int(sys.argv[6])
-except:
-    print "[colorPlane] not specified; using default= grayscale"
-
-try:
-    ratio = float(sys.argv[7])
-except:
-    print "[ratio] not specified; using default="  + str(ratio)
-
-#image=cv.LoadImage("eurotext.jpg", cv.CV_LOAD_IMAGE_GRAYSCALE)
-# @@@ try if we need to use a certain color plane
-if 0<=colorPlane<=2:
-    print "Using color plane " +str(colorPlane)
-    imgPageLayout = cv2.split(cv2.imread(frameFileName))[colorPlane]
-else:
-    print "No valid color plane specified; use all colors. " +str(colorPlane)
-    imgPageLayout = cv2.imread(frameFileName, cv.CV_LOAD_IMAGE_GRAYSCALE)
-
-# if no x y w h is specified, use the whole image
-if h<0:
-    x=0; y=0;
-    w=imgPageLayout.shape[0]
-    h=imgPageLayout.shape[1]
-    
-
-cv2.imshow("Frame", imgPageLayout)
-tmp=np.copy(imgPageLayout[y:y+h, x:x+w ])
-#cv2.imshow("cut", tmp)
-image = cv2.resize(tmp,(0,0), fx= ratio, fy=ratio )
-#cv2.imshow("resize", image)
-#print image.shape
-
-# Nope, Tesseract doesn't want a cvMAT
-#cvImage = cv.CreateMat(image.shape[0], image.shape[1], cv.CV_32FC1)
-#cvImage = cv.fromarray(image)
-#cv.ShowImage("CV Image", cvImage)
-#print cvImage.shape
-
-# Tesseract wants a grayscale IPLimage file. Can't use RGB or numpy array.
-bitmap = cv.CreateImageHeader((image.shape[1], image.shape[0]), cv.IPL_DEPTH_8U, 1)
-cv.SetData(bitmap, image.tostring(), image.dtype.itemsize * 1 * image.shape[1])
-
-#cv.ShowImage("IplImage", bitmap)
-
-
-# cvImage = cv.LoadImage("pagelayout.png", cv.CV_LOAD_IMAGE_GRAYSCALE)
-
-api = tesseract.TessBaseAPI()
-api.Init(".","eng",tesseract.OEM_DEFAULT)
-#api.SetPageSegMode(tesseract.PSM_SINGLE_WORD)
-api.SetPageSegMode(tesseract.PSM_AUTO)
-tesseract.SetCvImage(bitmap,api)
-text=api.GetUTF8Text()
-conf=api.MeanTextConf()
-# GetBoxText(page#)
-boxen = api.GetBoxText(0)
-html=api.GetHOCRText(0)
-#image=None
-print "Recognized Text = "
-print text
-print "Confidence level:" +str(conf)
-
-s=["title", "id", "class"]
 # create a subclass and override the handler methods
-class MyHTMLParser(HTMLParser):
+class MyHTMLParser(TessEngine.HTMLParser):
     box=None
     word=""
+    image=None
     def handle_starttag(self, tag, attrs):
         # print "Start tag:", tag
         #self.box = None
@@ -116,7 +29,7 @@ class MyHTMLParser(HTMLParser):
                     self.box=None
         # now draw box
         if self.box is not None:
-            cv2.rectangle(image,(self.box[0], self.box[1]),(self.box[2], self.box[3]),(0,128,0),1)
+            cv2.rectangle(self.image,(self.box[0], self.box[1]),(self.box[2], self.box[3]),(0,128,0),1)
         else:
             #print "box is not valid"
             pass
@@ -127,7 +40,7 @@ class MyHTMLParser(HTMLParser):
     def handle_data(self, data):
         # print data
         if self.box is not None:
-            cv2.putText(image, data, (self.box[0], self.box[1]), cv2.FONT_HERSHEY_PLAIN, 1.0, 0, thickness=1)
+            cv2.putText(self.image, data, (self.box[0], self.box[1]), cv2.FONT_HERSHEY_PLAIN, 1.0, 0, thickness=1)
         pass
     def handle_comment(self, data):
         print "Comment  :", data
@@ -135,7 +48,7 @@ class MyHTMLParser(HTMLParser):
         c = unichr(name2codepoint[name])
         #print "Named ent:", c
         if self.box is not None:
-            cv2.putText(image, c, (self.box[0], self.box[1]), cv2.FONT_HERSHEY_PLAIN, 1.0, 0, thickness=1)
+            cv2.putText(self.image, c, (self.box[0], self.box[1]), cv2.FONT_HERSHEY_PLAIN, 1.0, 0, thickness=1)
         pass
         
     def handle_charref(self, name):
@@ -145,18 +58,98 @@ class MyHTMLParser(HTMLParser):
             c = unichr(int(name))
         #print "Num ent  :", c
         if self.box is not None:
-            cv2.putText(image, c, (self.box[0], self.box[1]), cv2.FONT_HERSHEY_PLAIN, 1.0, 0, thickness=1)
+            cv2.putText(self.image, c, (self.box[0], self.box[1]), cv2.FONT_HERSHEY_PLAIN, 1.0, 0, thickness=1)
         pass
             
     def handle_decl(self, data):
         print "Decl     :", data
 
-# instantiate the parser and fed it some HTML
-parser = MyHTMLParser()
-parser.feed(html)
+def main(frameFileName, x, y, w, h, ratio, colorPlane):
+    
+    tess = TessEngine.TessEngine()
+    
+    #image=cv.LoadImage("eurotext.jpg", cv.CV_LOAD_IMAGE_GRAYSCALE)
+    # @@@ try if we need to use a certain color plane
+    if 0<=colorPlane<=2:
+        print "Using color plane " +str(colorPlane)
+        imgPageLayout = cv2.split(cv2.imread(frameFileName))[colorPlane]
+    else:
+        print "No valid color plane specified; use all colors. " +str(colorPlane)
+        imgPageLayout = cv2.imread(frameFileName, cv.CV_LOAD_IMAGE_GRAYSCALE)
 
-#cv.ShowImage("IplImage", bitmap)
-cv2.imshow("Boxen", image)
-cv2.waitKey()
-cv2.destroyAllWindows()
+    # if no x y w h is specified, use the whole image
+    if h<0:
+        x=0; y=0;
+        w=imgPageLayout.shape[0]
+        h=imgPageLayout.shape[1]
+        
 
+    cv2.imshow("Frame", imgPageLayout)
+    tmp=np.copy(imgPageLayout[y:y+h, x:x+w ])
+    #cv2.imshow("cut", tmp)
+    image = cv2.resize(tmp,(0,0), fx= ratio, fy=ratio )
+    #cv2.imshow("resize", image)
+    #print image.shape
+
+    ocrtext=tess.image2txt(image).replace("\n", " ")
+
+    html=tess.getHtml() # log the HTML values
+
+    #image=None
+    print "Recognized Text = "
+    print '"'+ocrtext+'"'
+    print "Confidence level= {}".format(tess.getConfidence())
+
+    #s=["title", "id", "class"]
+
+    # instantiate the parser and fed it some HTML
+    parser = MyHTMLParser()
+    parser.image = image
+    parser.feed(html)
+
+    #cv.ShowImage("IplImage", bitmap)
+    cv2.imshow("Boxen", image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+    return ocrtext
+
+if __name__ == "__main__":
+    # unit testing
+
+    # how much magnification for Tesseract to do OCR, typically 3-5
+    ratio=3.0
+    # where the text is. Default the whole page
+    w= -1
+    h= -1
+    x= -1
+    y= -1
+    colorPlane=-1
+    os.chdir(os.path.dirname(sys.argv[0]))
+
+    try:
+        frameFileName = sys.argv[1]
+    except:
+        print "testtess.py imageFileName [x y w h] [ratio] [colorPlane]"
+        exit(0)
+        
+    try:
+        x = int(sys.argv[2])
+        y = int(sys.argv[3])
+        w = int(sys.argv[4])
+        h = int(sys.argv[5])
+    except:
+        print "[x y w h] not specified; using the whole page"
+
+    try:
+        colorPlane = int(sys.argv[6])
+    except:
+        print "[colorPlane] not specified; using default= grayscale"
+
+    try:
+        ratio = float(sys.argv[7])
+    except:
+        print "[ratio] not specified; using default="  + str(ratio)
+
+    
+    main(frameFileName, x, y, w, h, ratio, colorPlane)
